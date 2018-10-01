912c9c51b1ff1f2454c23444200c17e1f263fc3e
Marnix Maas <Marnix.Maas@radboudumc.nl>
2018-10-01 11:07:54 +0200

    reconGriddingGrasp: added option to choose between coil combination modes 'sum of squares' or the method described in Walsh, MRM 2000, as implemented by Ricardo Otazo in adapt_array_2d. Probably similar to Siemens' 'Adaptive Combine' mode.

--- matlabtools/tools/reconGriddingGrasp.m
+++ matlabtools/tools/reconGriddingGrasp.m
@@ -1,4 +1,4 @@
-function out_img = reconGriddingGrasp(kdata)
+function out_img = reconGriddingGrasp(kdata, coilCombineMode)
 % Function for performing a reconstruction of single-echo golden-angle 
 % radial sparse (GRASP) k-space data using non-cartesian Fourier 
 % transformations ('gridding' reconstruction). No binning in different
@@ -6,10 +6,19 @@ function out_img = reconGriddingGrasp(kdata)
 % NOTE: it is assumed that a fourier transform along the slice direction
 % has already been performed!
 % Input parameters:
-% kdata: contains the raw k-space data. Must have dimensions
-% [nx,nc,ntviews,nz], which is the same order they are read in by mapVBVD.
+% - kdata: contains the raw k-space data. Must have dimensions
+%          [nx,nc,ntviews,nz], which is the same order they are read in by 
+%          mapVBVD.
+% - coilCombineMode: 
+%          method for combining signals from the different coil
+%          elements. 0 = sum-of-squares (default), 1 = using Ricardo Otazo's
+%          adapt_array_2d (unsure whether this is the same as Siemens' 'Adaptive
+%          Combine' option
 % Adapted from the demo file 'iterative_multicoil.m' by Tobias Block
 % by Marnix Maas (Marnix.Maas@radboudumc.nl), August 2018
+
+% Parse inputs
+if nargin<2, coilCombineMode = 0; end
 
 % Get data dimensions
 [nx,nc,ntviews,nz]=size(kdata);
@@ -26,23 +35,29 @@ fftSize=[nx,nx];
 FT = NUFFT(Traj(:,1:end), 1, 1, [0,0], fftSize, 2);
 
 % Create image to sum the gridded channels (for sum-of-squares combination)
-slices = 1:nz;
-out_img=single(zeros(nx,nx,length(slices)));
-for sl=slices
-    fprintf('Starting Gridding recon of slice %d / %d\n', (sl-slices(1)+1), length(slices));
+out_img=single(zeros(nx,nx,nz));
+% out_img=single(zeros(nx,nx,nz,nc));
+for sl=1:nz
+    fprintf('Starting Gridding recon of slice %d / %d\n', sl, nz);
     % Loop over channels and calculate image for each coil
+    workImage = zeros(nx,nx,nc);
     for channel=1:nc
         % Fetch rawdata for slice and channel and multiply with density
         % compensation function
-        workKData=dcf.*double(squeeze(kdata(:,channel,:,sl-(slices(1)-1))));
+        workKData=dcf.*double(squeeze(kdata(:,channel,:,sl)));
         
         % Run the NUFFT
-        workImage=FT'*(workKData);
-        
-        % Add squared channel image to buffer
-        out_img(:,:,sl-(slices(1)-1)) = out_img(:,:,sl-(slices(1)-1)) + abs(workImage.*workImage);
+        workImage(:,:,channel) = FT'*(workKData);
+    end
+    % Combine coil elements
+    if coilCombineMode
+        % "Adaptive combine" coil combination
+        [~, out_img(:,:,sl)] = adapt_array_2d(workImage);
+    else
+        % Sum of squares combination
+        out_img(:,:,sl) = sum(abs(workImage.*workImage),3);
     end
 end
-% Calculate the root (as final part of the sum-of-squares calculation)
-out_img = sqrt(out_img);
-out_img = out_img./max(out_img(:));
+
+% Normalize image
+out_img = out_img./max(abs(out_img(:)));
